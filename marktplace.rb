19054f8cbf33cd111f652e468c31e64a5011dedd
rubensrvsc <rubensspfc100@gmail.com>
2022-11-12 20:58:20 -0300

    refactor: change summarize position

--- marktplace.rb
+++ marktplace.rb
@@ -1,26 +1,27 @@
 
-summarize = 0
-
 def menu
     puts "[ 1 ] Comprar"
     puts "[ 2 ] Sair"
     option = gets.chomp.to_i
 end
 
-def select_option(option)
+def select_option
     products = {
         'Calça' => 350.0,
         'Camisa' => 200.0,
         'Tênis' => 650.0
     }
 
-    return products['Calça'] if option == 1
-    return products['Camisa'] if option == 2
-    return products['Camisa'] if option == 3
+    return products['Calça'] if select_clothe == 1
+    return products['Camisa'] if select_clothe == 2
+    return products['Tênis'] if select_clothe == 3
 end
+
+summarize = 0
 
 while true
     option_menu = menu
-    select_option(option_menu) if option_menu == 1
+    value = select_option if option_menu == 1
     break if option_menu == 2
+    summarize += value
 end
